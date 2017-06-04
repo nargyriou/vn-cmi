ee2614d40479654e96932a8e72cc51f8b0c77d19
caswitch <mariefrance.kom@gmail.com>
2017-06-05 00:36:53 +0200

    Push de la honte

--- dialogue.lua
+++ dialogue.lua
@@ -4,11 +4,12 @@ function Texte:new()
 	self.nbrPropositions = 1
 end
 
-function Texte:check()
-	A.text = love.graphics.newText(font, "A")
-	B.text = love.graphics.newText(font, "B")
-	C.text = love.graphics.newText(font, "C")
-	D.text = love.graphics.newText(font, "D")
+local A, B, C, D
+function Texte:affiche()
+	A = love.graphics.newText(font, "A")
+	B = love.graphics.newText(font, "B")
+	C = love.graphics.newText(font, "C")
+	D = love.graphics.newText(font, "D")
 end
 
 return Texte
