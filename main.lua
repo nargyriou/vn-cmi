8d6f84997ec5b14bde87d7961196026bd5edddc4
Wyvern67 <nicolas.argyriou95@gmail.com>
2017-06-04 16:05:24 +0200

    initial commit

---
+++ main.lua
@@ -0,0 +1,19 @@
+
+function love.load()
+	Object = require "classic"
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.textinput(text)
+end
+
+function love.mousepressed(x, y, button)  
+end
+
+function love.mousereleased(x, y, button)
+end
